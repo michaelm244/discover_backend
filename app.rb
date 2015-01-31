07cd340de4cfe049d2d2e56ca07b48b4da4986bf
michaelm244 <michaelm244@gmail.com>
2015-01-31 17:59:54 -0500

    init

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'nyny'
+require 'json'
+
+class App < NYNY::App
+  get '/' do
+    'wassup cuh'
+  end
+
+  post '/data_post' do
+    filename = (0...8).map { (65 + rand(26)).chr }.join
+    data = JSON.parse request.body.read
+    user_id = data["user_id"]
+
+    puts "user_id: #{user_id}"
+
+    if !File.directory? user_id
+      # make directory for user_id
+      Dir.mkdir user_id
+    end
+
+    File.open("#{user_id}/#{filename}", 'w') { |f|
+      f.write(request.body.read)
+    }
+
+    puts "wrote to file #{filename}"
+    puts request.body.read
+  end
+end
+
+App.run!
